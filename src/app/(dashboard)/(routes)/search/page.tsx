46132aa31dca6434c9c7114aa6520144726d7e4f
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-06-13 11:14:23 +0530

    feat: search query (debouncing)

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db";
 import Categories from "./_components/Categories";
+import SearchInput from "@/components/SearchInput";
 
 async function SearchPage() {
   const categories = await db.category.findMany({
@@ -8,9 +9,14 @@ async function SearchPage() {
     },
   });
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   );
 }
 
--- src/components/NavbarRoutes.tsx
+++ src/components/NavbarRoutes.tsx
@@ -4,25 +4,38 @@ import React from "react";
 import { Button } from "./ui/button";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { LogOut } from "lucide-react";
+import SearchInput from "./SearchInput";
 
 function NavbarRoutes() {
   const pathname = usePathname();
 
   const isPublisher = pathname.startsWith("/publisher");
   const isPlayer = pathname.startsWith("/course");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div>
-      {isPublisher ? (
-        <Link href={"/"}>
-          <Button>Exit</Button>
-        </Link>
-      ) : (
-        <Link href={"/publisher/courses"}>
-          <Button>Publisher mode</Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isPublisher || isPlayer ? (
+          <Link href={"/"}>
+            <Button>
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href={"/publisher/courses"}>
+            <Button>Publisher mode</Button>
+          </Link>
+        )}
+      </div>
+    </>
   );
 }
 
