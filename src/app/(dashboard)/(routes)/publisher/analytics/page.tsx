880366e248bfee8d0ba80ca66315981c6a60e321
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-06-17 09:52:14 +0530

    feat: teacher analytics dashboard

--- src/app/(dashboard)/(routes)/publisher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/publisher/analytics/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { getAnalytics } from "@/actions/get-analytics";
+import { redirect } from "next/navigation";
+import DataCard from "./_components/DataCard";
+import Chart from "./_components/Chart";
 
-function Page() {
-  return <div>This is analytics page</div>;
-}
+const AnalyticsPage = async () => {
+  const userId = "1";
 
-export default Page;
+  if (!userId) {
+    return redirect("/");
+  }
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+        <Chart data={data} />
+      </div>
+    </div>
+  );
+};
+
+export default AnalyticsPage;
