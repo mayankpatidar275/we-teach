1c7e816ffbd447698f2609510f419dbd28805e13
Mayank Patidar <154582476+thinkmetal-mayank@users.noreply.github.com>
2024-07-19 11:06:49 +0530

    fix: link logo to "/"

--- src/app/(dashboard)/_components/Logo.tsx
+++ src/app/(dashboard)/_components/Logo.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Logo(): React.ReactNode {
-  return <Image src={"/logo.svg"} alt={"logo"} height={150} width={220} />;
+  return (
+    <Link href="/">
+      <Image src={"/logo.svg"} alt={"logo"} height={150} width={220} />
+    </Link>
+  );
 }
 
 export default Logo;
