b18d9499bd1375e9d224f299063f386c2b14df65
Mayank Patidar <154582476+thinkmetal-mayank@users.noreply.github.com>
2024-07-22 11:06:19 +0530

    fix: upload route config added (issue in uploading: not solved yet)

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -8,4 +8,12 @@ export const { GET, POST } = createRouteHandler({
  
   // Apply an (optional) custom config:
   // config: { ... },
+    config: {
+    callbackUrl: "https://we-teach.mkpatidar.in",
+    uploadthingId: process.env.UPLOADTHING_APP_ID,
+    uploadthingSecret: process.env.UPLOADTHING_SECRET,
+    logLevel: "info",
+    // isDev: process.env.NODE_ENV === "development" || process.env.NODE_ENV === "dev",
+    fetch: globalThis.fetch,
+  },
 });
