c4d68e5c724dfb14d8a4ad46545b4c6962f55f87
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-10-15 17:43:52 +0530

    fix: uploadthing callbackUrl, vercel-production.env

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,15 +1,18 @@
 import { createRouteHandler } from "uploadthing/next";
- 
+
 import { ourFileRouter } from "./core";
- 
+
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
- 
+
   // Apply an (optional) custom config:
   // config: { ... },
-    config: {
-    callbackUrl: "https://we-teach.mkpatidar.in",
+  config: {
+    callbackUrl:
+      process.env.NEXT_PUBLIC_PLATFORM === "vps"
+        ? "https://we-teach.mkpatidar.in"
+        : "https://we-teach.vercel.app",
     uploadthingId: process.env.UPLOADTHING_APP_ID,
     uploadthingSecret: process.env.UPLOADTHING_SECRET,
     logLevel: "info",
